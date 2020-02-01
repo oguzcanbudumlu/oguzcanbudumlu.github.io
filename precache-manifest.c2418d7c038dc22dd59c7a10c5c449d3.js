d31b2609898961c30ee09d087bb5bd3f2f7ec05d
OGUZCAN BUDUMLU <e2098820@ceng.metu.edu.tr>
2020-02-02 01:57:17 +0300

    Updates

---
+++ precache-manifest.c2418d7c038dc22dd59c7a10c5c449d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fed537b95298c61558d3c7d49f7eec7",
+    "url": "/oguzcanbudumlu.github.io/index.html"
+  },
+  {
+    "revision": "c591775fe2e4aa1fa248",
+    "url": "/oguzcanbudumlu.github.io/static/css/main.66490977.chunk.css"
+  },
+  {
+    "revision": "a1599e5425831808610c",
+    "url": "/oguzcanbudumlu.github.io/static/js/2.e2ff2414.chunk.js"
+  },
+  {
+    "revision": "bc0c83fb5c6c8b946b94c2ec61dd7b90",
+    "url": "/oguzcanbudumlu.github.io/static/js/2.e2ff2414.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c591775fe2e4aa1fa248",
+    "url": "/oguzcanbudumlu.github.io/static/js/main.3a56587d.chunk.js"
+  },
+  {
+    "revision": "66ad8faae6c2dfbe0174",
+    "url": "/oguzcanbudumlu.github.io/static/js/runtime-main.1f4aa772.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oguzcanbudumlu.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.593fbf68716ac41aaed89aca206f758e.js"
+  "/oguzcanbudumlu.github.io/precache-manifest.c2418d7c038dc22dd59c7a10c5c449d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oguzcanbudumlu.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
